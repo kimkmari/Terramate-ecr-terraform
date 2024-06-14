9611df6e88bb19e6a54a6c4bbce4e2d4224d24b2
kimkmari <kimkmari04@gmail.com>
2024-06-14 13:29:02 +0900

    terramate-wrapper.sh 추가 write 권한 재 추가

---
+++ terramate-wrapper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+tmpdir=$(mktemp -d)
+
+stdout="${tmpdir}/stdout.txt"
+stderr="${tmpdir}/stderr.txt"
+
+terramate-bin "$@" > >(tee "${stdout}") 2> >(tee "${stderr}" >&2)
+
+exitcode=$?
+
+echo "exitcode=${exitcode}" >> "$GITHUB_OUTPUT"
+
+echo 'stdout<<EOF' >> "$GITHUB_OUTPUT"
+cat "${stdout}" >> "$GITHUB_OUTPUT"
+echo EOF >> "$GITHUB_OUTPUT"
+
+echo 'stderr<<EOF' >> "$GITHUB_OUTPUT"
+cat "${stderr}" >> "$GITHUB_OUTPUT"
+echo EOF >> "$GITHUB_OUTPUT"
+
+exit ${exitcode}
